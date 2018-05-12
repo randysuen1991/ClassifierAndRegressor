effd6ec1e07d7c3d36e8294afab1dfa6cba6e52c
randysuen1991 <randysuen1991@gmail.com>
2018-05-12 19:50:11 +0800

    new

--- Classifier.py
+++ Classifier.py
@@ -53,18 +53,23 @@ class TwoStepClassifier(Classifier):
     def Fit(self,X_train,Y_train,**kwargs):
 
         self.input_shape = (X_train.shape[1],X_train.shape[2],X_train.shape[3])
-        dimension = self._Search_Dimensionality(X_train=X_train,Y_train=Y_train)
-        linear_subspace = self.first_step_function(X_train=X_train,Y_train=Y_train,p_tilde=dimension,q_tilde=dimension)
-        self.parameters['first_step']['column'] = linear_subspace[0]
-        self.parameters['first_step']['row'] = linear_subspace[1]
+        dimension = self._Search_Dimensionality(X_train = X_train,
+                                                Y_train = Y_train)
+        linear_subspace = self.first_step_function(X_train = X_train,
+                                                   Y_train = Y_train,
+                                                   p_tilde = dimension,
+                                                   q_tilde = dimension)
         
-        X_train_proj = np.matmul(X_train,self.parameters['first_step']['row'])
-        X_train_proj = np.matmul(self.parameters['first_step']['column'],X_train_proj)
+        self.parameters['first_step']['row'] = linear_subspace[0]
+        self.parameters['first_step']['column'] = linear_subspace[1]
+        
+        X_train_proj = DRA.MultilinearReduction.TensorProject(X_train,self.parameters['first_step']['row'],self.parameters['first_step']['column'])
         X_train_proj_vec = UF.imgs2vectors(X_train_proj)
         
         self.parameters['second_step'] = self.second_step_function(X_train=X_train,Y_train=Y_train)
-        self.transformed_X_train = np.matmul(X_train_proj_vec,self.parameters['second_step'])
         
+        self.transformed_X_train = np.matmul(X_train_proj_vec,self.parameters['second_step'])
+        self.Y_train = Y_train
         return self.parameters
     
     def _Search_Dimensionality(self,X_train,Y_train,p_tilde,q_tilde,dimension=50):
@@ -86,19 +91,22 @@ class TwoStepClassifier(Classifier):
         return index + 1
     
     
-    def Classify(self,X_train,Y_train,X_test,Y_test,**kwargs):
+    def Classify(self,X_test,Y_test,**kwargs):
         k = kwargs.get('k',1)
         # Use K-nearest neighbor to classify the testing data
         neighbor = KNeighborsClassifier(n_neighbors=k)
         neighbor.fit(self.transformed_X_train,self.Y_train.ravel())
         
-        results = neighbor.predict(X_test_proj)
+        X_test_proj = DRA.MultilinearReduction.TensorProject(X_test,self.parameters['first_step']['row'],self.parameters['first_step']['column'])
+        X_test_proj_vec = UF.imgs2vectors(X_test_proj)
+        X_test_proj_vec_proj = np.matmul(X_test_proj_vec,self.parameters['second_step'])
+        results = neighbor.predict(X_test_proj_vec_proj)
         correct_results = np.where(results == Y_test.ravel())[0]
         return len(correct_results) / len(Y_test), correct_results
 
     def Compute_Ratio(self,X_train,Y_train):
         Total_centered = DRA.TotalCentered(X_train)
-        Betweeen_centered = DRA.BetweenGroupMeanCentered(X_train,Y_train)
+        Between_centered = DRA.BetweenGroupMeanCentered(X_train,Y_train)
         _, S, _ = np.linalg.svd(Total_centered,full_matrices=False)
         denominator = np.sum(S)
         _, S, _ = np.linalg.svd(Between_centered,full_matrices=False)
