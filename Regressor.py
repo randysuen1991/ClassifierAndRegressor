3416af3710311c16f835afa8c521b5c373fc24c8
randysuen1991 <randysuen1991@gmail.com>
2018-05-24 16:27:53 +0800

    new

--- Regressor.py
+++ Regressor.py
@@ -8,18 +8,6 @@ class Regressor():
         self.parameters = None
         self.regressor = None
         
-    def Fit(self):
-        raise NotImplementedError
-    
-    def Predict(self):
-        raise NotImplementedError
-        
-
-class OrdianryLeastSquareRegressor(Regressor):
-    def __init__(self):
-        super().__init__()
-        self.regressor = LinearRegression()
-        
     def Fit(self,X_train,Y_train):
         self.regressor.fit(X_train,Y_train)
         # this function would return the parameters of the regressor and the R^2.
@@ -27,6 +15,14 @@ class OrdianryLeastSquareRegressor(Regressor):
     def Predict(self,X_test):
         prediction = self.regressor.predict(X_test)
         return prediction
+        
+
+class OrdianryLeastSquareRegressor(Regressor):
+    def __init__(self):
+        super().__init__()
+        self.regressor = LinearRegression()
+        
+    
       
 class PartialLeastSqaureRegressor(Regressor):
     def __init__(self):
@@ -41,23 +37,21 @@ class PartialLeastSqaureRegressor(Regressor):
 class LassoRegressor(Regressor):
     def __init__(self):
         super().__init__()
-        
-    def Fit(self,X_train,Y_train,n_components):
-        pass
-    
-    def Predict(self):
-        pass
+        self.regressor = Lasso()
     
     
 class PrincipalComponentRegressor(Regressor):
-    def __init__(self):
+    def __init__(self,n_components):
         super().__init__()
-        
+        self.regressor = OrdianryLeastSquareRegressor()
+        self.pca = PCA(n_components)
     def Fit(self,X_train,Y_train,n_components):
-        pass
-    
-    def Predict(self):
-        pass
+        X_train_transform = self.pca.fit_transform(X_train)
+        self.regressor.Fit(X_train_transform,Y_train)
+    def Predict(self,X_test):
+        X_test_transform = self.pca.fit_transform(X_test)
+        prediction = self.regressor.Predict(X_test_transform)
+        return prediction
     
 class RidgeRregressor(Regressor):
     def __init__(self):
