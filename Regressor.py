c4d11ccb47c97b0af05977184caaf32b7c15dba4
randysuen1991 <randysuen1991@gmail.com>
2018-05-24 15:56:50 +0800

    new

--- Regressor.py
+++ Regressor.py
@@ -1,11 +1,12 @@
 import numpy as np
 from sklearn.cross_decomposition import PLSRegression
 from sklearn.decomposition import PCA
-from sklearn.linear_model import Lasso, Ridge
+from sklearn.linear_model import Lasso, Ridge, LinearRegression
 
 class Regressor():
     def __init__(self):
         self.parameters = None
+        self.regressor = None
         
     def Fit(self):
         raise NotImplementedError
@@ -13,7 +14,20 @@ class Regressor():
     def Predict(self):
         raise NotImplementedError
         
+
+class OrdianryLeastSquareRegressor(Regressor):
+    def __init__(self):
+        super().__init__()
+        self.regressor = LinearRegression()
         
+    def Fit(self,X_train,Y_train):
+        self.regressor.fit(X_train,Y_train)
+        # this function would return the parameters of the regressor and the R^2.
+        return self.regressor.get_params(), self.regressor.score 
+    def Predict(self,X_test):
+        prediction = self.regressor.predict(X_test)
+        return prediction
+      
 class PartialLeastSqaureRegressor(Regressor):
     def __init__(self):
         super().__init__()
@@ -21,7 +35,7 @@ class PartialLeastSqaureRegressor(Regressor):
     def Fit(self,X_train,Y_train,n_components):
         pass
     
-    def Predict(self):
+    def Predict(self,X_test):
         pass
     
 class LassoRegressor(Regressor):
@@ -51,6 +65,8 @@ class RidgeRregressor(Regressor):
         
     def Fit(self,X_train,Y_train,n_components):
         pass
+    
+
     
     def Predict(self):
         pass
