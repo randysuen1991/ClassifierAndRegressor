1b182464b5691e6a8b0880a392ea6c9ad8b19285
孫以恒-新金融商品部-策略交易處 <randysuen@masterlink.com.tw>
2018-06-06 14:30:44 +0800

    new

--- ModelSelection.py
+++ ModelSelection.py
@@ -4,13 +4,15 @@ import sys
 if 'C:\\Users\\ASUS\Dropbox\\pycode\\mine\\Dimension-Reduction-Approaches' not in sys.path :
     sys.path.append('C:\\Users\\ASUS\Dropbox\\pycode\\mine\\Dimension-Reduction-Approaches')
 from DimensionReductionApproaches import CenteringDecorator
-
+import warnings
 
 
 class ModelSelection():
     # Till now, Y_train should be a N*1 matrix.
     @CenteringDecorator
     def CovSelection(X_train,Y_train):
+        if Y_train.shape[1] > 1 :
+            warnings.warn('The dimension of the Y variable should be 1 now.')
         covariance = np.matmul(X_train.T,Y_train)
         
         
