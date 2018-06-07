004c32aaeb11f1598b346921632ff67df6d6c701
randysuen1991 <randysuen1991@gmail.com>
2018-06-07 14:01:37 +0800

    new

--- ModelSelection.py
+++ ModelSelection.py
@@ -9,11 +9,14 @@ import warnings
 class ModelSelection():
     # Till now, Y_train should be a N*1 matrix.
     @CenteringDecorator
-    def CovSelection(X_train,Y_train):
+    def CorrSelection(X_train,Y_train):
         if Y_train.shape[1] > 1 :
             warnings.warn('The dimension of the Y variable should be 1 now.')
         covariance = np.matmul(X_train.T,Y_train)
-        sorted_index = np.argsort(covariance)
+        X_std = np.std(X_train,axis=0)
+        Y_std = np.std(Y_train)
+        corr = covariance/(X_std*Y_std)
+        sorted_index = np.argsort(corr)
         return sorted_index[::-1]
         
     def StepSelection():
