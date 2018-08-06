5f9afa888193776e4f034e224e800c2b846a71f3
randysuen1991 <randysuen1991@gmail.com>
2018-08-06 09:34:36 +0800

    new

--- Regressor.py
+++ Regressor.py
@@ -216,6 +216,7 @@ class ForwardStepwiseRegressor(Regressor):
     def Fit(self, X_train, Y_train, **kwargs):
         ids = MS.ModelSelection.FowardSelection(model=OrdinaryLeastSquaredRegressor, X_train=X_train,
                                                 Y_train=Y_train, p=kwargs.get('p', X_train.shape[1]))
+        print(ids)
         self.X_train = X_train[:, ids]
         self.Y_train = Y_train
         self.regressor.fit(self.X_train, self.Y_train)
@@ -234,6 +235,7 @@ class BackwardStepwiseRegressor(Regressor):
     def Fit(self, X_train, Y_train, **kwargs):
         ids = MS.ModelSelection.BackwardSelection(model=OrdinaryLeastSquaredRegressor, X_train=X_train,
                                                   Y_train=Y_train, p=kwargs.get('p', X_train.shape[1]))
+        print(ids)
         self.X_train = X_train[:, ids]
         self.Y_train = Y_train
 
