79f6d8149d70b1bb5429a70a9e5675d616f29585
randysuen1991 <randysuen1991@gmail.com>
2018-09-26 17:49:46 +0800

    new

--- AdditiveModel/AdditiveModel.py
+++ AdditiveModel/AdditiveModel.py
@@ -9,9 +9,9 @@ class AdditiveModel:
         self.alpha = None
         self.smoothers = None
 
-    def fit(self, x_train, y_train):
+    def fit(self, x_train, y_train, threshold):
         self.alpha, self.smoothers = self.backfitting(x_train=x_train, y_train=y_train, smoother=self.smoother,
-                                                      smooth_factor=self.smoother_factor)
+                                                      smooth_factor=self.smoother_factor, threshold=threshold)
 
     def predict(self, x_test):
         results = self.alpha
@@ -35,9 +35,7 @@ class AdditiveModel:
                 y = y_train - alpha
                 for j in range(x_train.shape[1]):
                     if j != i and (c > 0 or j < i):
-                        # print('residual computing...', j)
                         y -= (smoothers[j].predict(x_test=x_train[:, j]) - means[j])
-
                 smoothers[i].fit(x_train=x_train[:, i], y_train=y)
                 means[i] = np.mean(smoothers[i].predict(x_train[:, i]))
             if not first:
@@ -48,19 +46,20 @@ class AdditiveModel:
             smoothers_old = cp.deepcopy(smoothers)
             first = False
             c += 1
-            if c == 1000:
-                print('Not convergence, but attain upper bound of iteration times!')
+            if c == 2000:
+                print('Not convergence, but number of iteration times exceeds.')
                 return alpha, smoothers
 
     @staticmethod
     def _check_convergence(smoothers, smoothers_old, x_train, threshold):
         i = 0
+        res = 0
         for smoother, smoother_old in zip(smoothers, smoothers_old):
             results_old = smoother_old.predict(x_train[:, i])
             results = smoother.predict(x_train[:, i])
-            res_sum = np.sum(np.abs(results-results_old))
-            print('residual sum:', res_sum)
+            res += np.sum(results-results_old)
             i += 1
-            if res_sum > threshold:
-                return False
+        print('Residual:', np.abs(res))
+        if np.abs(res) > threshold:
+            return False
         return True
