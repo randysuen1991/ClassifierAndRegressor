75169602ad661f2814ae955d710289f7782c98cf
吳品毅-新金融商品部-策略交易處 <twwow@masterlink.com.tw>
2018-11-30 16:25:41 +0800

    test

--- example.py
+++ example.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 # In this example, I demonstrate different regressions methods. 
 def example1():
     
-    noise = np.random.normal(loc=0,scale=10,size=(100,))
-    X_train = np.random.randint(low=1,high=20,size=(100,))
+    noise = np.random.normal(loc=0, scale=10, size=(100,))
+    X_train = np.random.randint(low=1, high=20, size=(100,))
     Y_train = 1 + 2 * X_train + noise
-    
-    
+    a = 5
+    b = 4
 
 
 
